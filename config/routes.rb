8c8ed7fc687db30e6b83b2405948c71ba821177d
Aakanksha Patwa <aakankshapatwa36@gmail.com>
2025-06-07 22:09:09 +0530

    initial commit and create artists

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  devise_for :artists
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  authenticated :artist do
+    root "artists#dashboard", as: :authenticated_artist_root
+    resources :songs
+    resources :dashboard, to: "artists#dashboard"
+  end
+
+  root "home#index"
+
+  resources :music, only: [:show], controller: :music
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
